2dd8706fba7d27d71ead4a35ea83264f472e28a0
Lucas Henrique <lhs_azevedo@hotmail.com>
2020-10-21 23:51:33 -0300

    Traduz pra ingles e adiciona testes de subtração

---
+++ CalculadorTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use SebastianBergmann\Complexity\Calculator;
+
+require_once "Calculator.php";
+
+class CalculatorTest extends TestCase
+{
+    public function test_initialization()
+    {
+        $this->assertSame(1,    Calculator::init(1)->result());
+        $this->assertSame(2,    Calculator::init(2)->result());
+        $this->assertSame(10,   Calculator::init(10)->result());
+        $this->assertSame(0,    Calculator::init()->result());
+    }
+
+    public function test_basic_sum()
+    {
+        $this->assertSame(2,    Calculator::init(1)->plus(1)->result());
+        $this->assertSame(5,    Calculator::init(2)->plus(3)->result());
+        $this->assertSame(47,   Calculator::init(10)->plus(37)->result());
+        $this->assertSame(6,    Calculator::init(3)->plus(3)->result());
+        $this->assertSame(512,  Calculator::init(256)->plus(256)->result());
+        $this->assertSame(0,    Calculator::init(0)->plus(0)->result());
+    }
+
+    public function test_negative_sum()
+    {
+        $this->assertSame(-7,   Calculator::init(7)->plus(-14)->result());
+        $this->assertSame(-50,  Calculator::init(100)->plus(-150)->result());
+        $this->assertSame(-1,   Calculator::init(0)->plus(-1)->result());
+        $this->assertSame(-30,  Calculator::init(-50)->plus(20)->result());
+        $this->assertSame(42,   Calculator::init(-18)->plus(60)->result());
+        $this->assertSame(0,    Calculator::init(-50)->plus(50)->result());
+    }
+
+    public function test_basic_subtraction()
+    {
+        $this->assertSame(5,   Calculator::init(10)->minus(5));
+        $this->assertSame(0,   Calculator::init(1)->minus(1));
+        $this->assertSame(27,  Calculator::init(64)->minus(37));
+        $this->assertSame(-25, Calculator::init(18)->minus(43));
+        $this->assertSame(100, Calculator::init(999)->minus(899));
+        $this->assertSame(0,   Calculator::init(0)->minus(0));
+    }
+}
--- README.md
+++ README.md
@@ -6,22 +6,26 @@
 
 ## Instalação
 
-- Faça um fork deste repositório para a sua conta e clone na sua máquina.
-- Entre na pasta e rode o comando `composer install` para instalar as dependências.
-- Na raiz do projeto, crie um arquivo com o nome `Calculadora.php` e inicie a implementação da sua classe:
-    ```php
-    <?php
+Faça um fork deste repositório para a sua conta e clone na sua máquina.
 
-    class Calculadora
-    {
-        // ...
-    }
-    ```
+Entre na pasta e rode o comando `composer install` para instalar as dependências.
+
+Na raiz do projeto, crie um arquivo com o nome `Calculator.php` com a classe da sua futura calculadora:
+```php
+<?php
+
+class Calculator
+{
+    // ...
+}
+```
+
+Pronto, você já pode começar a implementar 🚀
 
 ## Executando os testes
 
 Execute `composer test` para executar a suite de testes. Ela já está configurada para encerrar no primeiro teste que falhar.
 
-## Atualizações
+## Atualizações futuras
 
-Tente não modificar outros arquivos além do Calculadora.php para evitar conflitos nas atualizações que ainda serão feitas neste repositório.
+Este desafio ainda está em desenvolvimento, então tente não modificar outros arquivos além do `Calculator.php` para evitar conflitos nas atualizações que ainda serão feitas neste repositório.
